f7a5e5e0c34ba14925c709c4347ee750587d46f4
givenone <givenone1@gmail.com>
2020-06-18 12:17:24 +0000

    fixed fpga_api

--- src/fpga_api.cpp
+++ src/fpga_api.cpp
@@ -51,7 +51,7 @@ void quantize(const float* input, int* quantized, int num_input, int bits_min, i
 {
   for(int i = 0; i < num_input; i++)
   {
-    quantized[i] = (char) ((unsigned int)(input[i] / scale)); // TODO: convert floating point to quantized value
+    quantized[i] = (int) ((input[i] / scale)); // TODO: convert floating point to quantized value
   }
 }
 
@@ -89,14 +89,14 @@ void FPGA::largeMV(const float *large_mat, const float *input, float *output, in
   int act_bits_max = (1<<(comp->act_bits-1))-1;
 
   float act_scale = (comp->act_min - comp->act_max) / act_bits_max; // TODO calculate the scale factor
-  char act_offset = (-1 * comp->act_min) / act_scale; // TODO calculate the zero-offset
+  int act_offset = (int) (-1 * comp->act_min) / act_scale; // TODO calculate the zero-offset
   quantize(input, qinput, num_input, act_bits_min, act_bits_max, act_offset, act_scale);
 
   int weight_bits_min = 0;
   int weight_bits_max = (1<<(comp->weight_bits-1))-1;
 
     float weight_scale = (comp->weight_max - comp->weight_min) / 127; // TODO calculate the scale factor
-    char weight_offset = (-1 * comp->weight_min) /weight_scale; // TODO calculate the zero-offset
+    int weight_offset = (int) (-1 * comp->weight_min) /weight_scale; // TODO calculate the zero-offset
   quantize(large_mat, qlarge_mat, num_input*num_output, weight_bits_min, weight_bits_max, weight_offset, weight_scale);
 
   // 0) Initialize output vector
